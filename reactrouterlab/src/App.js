6e0fb061df32a57933a3b152dbd238c8eb024582
Salim Mahdjane <mahdjanesalim4@gmail.com>
2023-05-16 14:22:46 +0200

    Update App.js: Updated UI and functionality for sensor data display and connection management

--- reactrouterlab/src/App.js
+++ reactrouterlab/src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import styles from "./App.module.css";
+import React from "react";
+import { connectToSocket, disconnectFromSocket } from "./dataHandling";
+import { Routes, Route, Link, Navigate } from "react-router-dom";
+import { DataDisplayer } from "./component/dataDisplayer";
 
 function App() {
+  const [sensorList, setSensorList] = React.useState([]);
+  const [queue, setQueue] = React.useState([]);
+  const [url, setUrl] = React.useState("");
+  const [connected, setConnected] = React.useState(false);
+  const [client, setClient] = React.useState(null);
+  
+  const onMessage = (message) => {
+    const topic = message.destinationName;
+    const payload = JSON.parse(message.payloadString);
+    if (sensorList.find((sensor) => sensor.topic === topic) === undefined) {
+      payload.value = [payload.value];
+      const copy = [...sensorList, { topic, payload }];
+      setQueue(copy);
+    }
+  };
+
+  const deconnect = () => {
+    setConnected(false);
+    disconnectFromSocket(client);
+    setQueue([]);
+    setSensorList([]);
+    setClient(null);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <div className={styles.Header}>
+        <img src={logo} alt="Stuff sensor" className={styles['App-logo']} />
+      </div>
+      <header className={styles['App-header']}>
+        <div className={styles['App-column-left']}>
+          <Routes>
+            <Route index element={<main className=""></main>} />
+            {sensorList.map((sensor) => {
+              return (
+                <Route
+                  key={sensor.topic}
+                  path={`/${sensor.payload.name}`}
+                  element={<DataDisplayer sensorData={sensor} />}
+                />
+              );
+            })}
+          </Routes>
+        </div>
+        <div className={styles['App-column-left']}>
+          <form
+            onSubmit={(evt) => {
+              evt.preventDefault();
+              if (connected) {
+                deconnect();
+                return false;
+              }
+              const cli = connectToSocket(url, onMessage);
+              if (cli === null) return false;
+              setClient(cli);
+              setConnected(true);
+              return false;
+            }}
+          >
+           
+            <input
+              onChange={(evt) => setUrl(evt.target.value)}
+              placeholder="Entrez l'url de la datasource"
+            />
+            <input
+              type="submit"
+              value={connected ? "Se dÃ©connecter" : "Se connecter"}
+            />
+          </form>
+          <>
+          {sensorList.map((sensor) => {
+    return (
+      <ul key={sensor.topic} className={styles['sensor-list']}>
+        <Link to={`/${sensor.payload.name}`}>
+          {sensor.payload.name}
+        </Link>
+      </ul>
+    );
+  })}
+          </>
+        </div>
       </header>
-    </div>
+    </>
   );
 }
-
-export default App;
